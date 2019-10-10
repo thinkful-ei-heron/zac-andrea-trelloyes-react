c036ee91d064361512f318c504b7a95d1ca90d38
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-10 11:46:25 -0500

    working on add card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
 import List from './List'
 import './App.css';
+import STORE from './STORE';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
+  // static defaultProps = {
+  //   store: {
+  //     lists: [],
+  //     allCards: {},
+  //   }
+  // };
+
+  state = {
+    store: STORE
+  }
+
+  handleAddCard = index => {
+    const newRandomCard = () => {
+      const id = Math.random().toString(36).substring(2, 4)
+        + Math.random().toString(36).substring(2, 4);
+      return {
+        id,
+        title: `Random Card ${id}`,
+        content: 'lorem ipsum',
+      };
     }
-  };
+    const newCard = newRandomCard();
+    const newId = newCard.id;
+    // const newIdObj = this.state.store.lists.find(item => {
+    //   item.id === id;
+    // });
+    console.log(index);
+
+    // const newSTORE = this.state.store;
+    // console.log(newSTORE);
+    // newSTORE.lists[0].cardIds.push(newId);
+    // newSTORE.allCards = {
+    //   newId: newCard
+    // };
+
+    const newStore = {...this.state.store};
+    newStore.lists[index - 1].cardIds.push(newId);
+    // newStore.allCards = {
+    //     newId: newCard
+    //   };
+
+    console.log(newStore);
+    this.setState({
+      store: newStore
+    });
+  }
 
   render() {
-    const { store } = this.props
+    const store = this.state.store;
+
+
     return (
       <main className='App'>
         <header className='App-header'>
@@ -21,8 +64,10 @@ class App extends Component {
           {store.lists.map(list => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              handleAddCard={this.handleAddCard}
             />
           ))}
         </div>
--- src/List.js
+++ src/List.js
@@ -3,20 +3,25 @@ import Card from './Card'
 import './List.css';
 
 export default function List(props) {
+  console.log(props);
+  console.log(props.id);
+  console.log(props.cards);
   return (
     <section className='List'>
       <header className='List-header'>
         <h2>{props.header}</h2>
       </header>
       <div className='List-cards'>
-        {props.cards.map((card) =>
+        {props.cards.map(card =>
           <Card
             key={card.id}
+            id={card.id}
             title={card.title}
             content={card.content}
           />
         )}
         <button
+          onClick={() => props.handleAddCard(props.id)}
           type='button'
           className='List-add-button'
         >
